5f979f0381eb979eb96ff068cfb5c15cafd2e183
Cody <cody.barrus@concur.com>
2016-10-23 11:48:23 -0700

    added upgrades to all improvements

--- src/utils/improvements.js
+++ src/utils/improvements.js
@@ -9,7 +9,7 @@ const improvements = [
     },
     upgrades: [
       {
-        label: 'Increase Maximum Resources (lvl 1)',
+        label: 'Upgrade Treasury (lvl 1)',
         code: 'lvl1',
         isPurchased: false,
         cost: {
@@ -17,7 +17,7 @@ const improvements = [
         }
       },
       {
-        label: 'Increase Maximum Resources (lvl 2)',
+        label: 'Upgrade Treasury (lvl 2)',
         code: 'lvl2',
         isPurchased: false,
         cost: {
@@ -25,7 +25,7 @@ const improvements = [
         }
       },
       {
-        label: 'Increase Maximum Resources (lvl 3)',
+        label: 'Upgrade Treasury (lvl 3)',
         code: 'lvl3',
         isPurchased: false,
         cost: {
@@ -33,7 +33,7 @@ const improvements = [
         }
       },
       {
-        label: 'Increase Maximum Resources (lvl 4)',
+        label: 'Upgrade Treasury (lvl 4)',
         code: 'lvl4',
         isPurchased: false,
         cost: {
@@ -41,7 +41,7 @@ const improvements = [
         }
       },
       {
-        label: 'Increase Maximum Resources (lvl 5)',
+        label: 'Upgrade Treasury (lvl 5)',
         code: 'lvl5',
         isPurchased: false,
         cost: {
@@ -59,7 +59,55 @@ const improvements = [
       gold: 200,
       iron: 50
     },
-    upgrades: []
+    upgrades: [
+      {
+        label: 'Upgrade Armory (lvl 1)',
+        code: 'lvl1',
+        isPurchased: false,
+        cost: {
+          gold: 500,
+          iron: 100
+        }
+      },
+      {
+        label: 'Upgrade Armory (lvl 2)',
+        code: 'lvl2',
+        isPurchased: false,
+        cost: {
+          gold: 1000,
+          iron: 200
+        }
+      },
+      {
+        label: 'Upgrade Armory (lvl 3)',
+        code: 'lvl3',
+        isPurchased: false,
+        cost: {
+          gold: 5000,
+          iron: 500
+        }
+      },
+      {
+        label: 'Upgrade Armory (lvl 4)',
+        code: 'lvl4',
+        isPurchased: false,
+        cost: {
+          gold: 10000,
+          iron: 2000,
+          crystals: 10
+        }
+      },
+      {
+        label: 'Upgrade Armory (lvl 5)',
+        code: 'lvl5',
+        isPurchased: false,
+        cost: {
+          gold: 25000,
+          iron: 5000,
+          crystals: 100
+        }
+      }
+    ]
   },
   {
     name: 'Cellar',
@@ -70,7 +118,60 @@ const improvements = [
       gold: 500,
       gin: 100
     },
-    upgrades: []
+    upgrades: [
+      {
+        label: 'Upgrade Cellar (lvl 1)',
+        code: 'lvl1',
+        isPurchased: false,
+        cost: {
+          gold: 1000,
+          iron: 50,
+          gin: 50
+        }
+      },
+      {
+        label: 'Upgrade Cellar (lvl 2)',
+        code: 'lvl2',
+        isPurchased: false,
+        cost: {
+          gold: 1500,
+          iron: 150,
+          gin: 150
+        }
+      },
+      {
+        label: 'Upgrade Cellar (lvl 3)',
+        code: 'lvl3',
+        isPurchased: false,
+        cost: {
+          gold: 5000,
+          iron: 500,
+          gin: 250
+        }
+      },
+      {
+        label: 'Upgrade Cellar (lvl 4)',
+        code: 'lvl4',
+        isPurchased: false,
+        cost: {
+          gold: 7500,
+          iron: 1000,
+          gin: 500,
+          crystals: 10
+        }
+      },
+      {
+        label: 'Upgrade Cellar (lvl 5)',
+        code: 'lvl5',
+        isPurchased: false,
+        cost: {
+          gold: 15000,
+          iron: 2000,
+          gin: 1000,
+          crystals: 150
+        }
+      }
+    ]
   },
   {
     name: 'Theives Guild',
@@ -82,7 +183,63 @@ const improvements = [
       gin: 10,
       iron: 30
     },
-    upgrades: []
+    upgrades: [
+      {
+        label: 'Upgrade Theives Guild (lvl 1)',
+        code: 'lvl1',
+        isPurchased: false,
+        cost: {
+          gold: 1500,
+          iron: 75,
+          gin: 75,
+          crystals: 10
+        }
+      },
+      {
+        label: 'Upgrade Theives Guild (lvl 2)',
+        code: 'lvl2',
+        isPurchased: false,
+        cost: {
+          gold: 3000,
+          iron: 300,
+          gin: 200,
+          crystals: 50
+        }
+      },
+      {
+        label: 'Upgrade Theives Guild (lvl 3)',
+        code: 'lvl3',
+        isPurchased: false,
+        cost: {
+          gold: 10000,
+          iron: 1000,
+          gin: 750,
+          crystals: 100
+        }
+      },
+      {
+        label: 'Upgrade Theives Guild (lvl 4)',
+        code: 'lvl4',
+        isPurchased: false,
+        cost: {
+          gold: 20000,
+          iron: 1500,
+          gin: 1000,
+          crystals: 200
+        }
+      },
+      {
+        label: 'Upgrade Theives Guild (lvl 5)',
+        code: 'lvl5',
+        isPurchased: false,
+        cost: {
+          gold: 50000,
+          iron: 2000,
+          gin: 1500,
+          crystals: 500
+        }
+      }
+    ]
   },
   {
     name: 'Science Lab',
@@ -94,7 +251,58 @@ const improvements = [
       iron: 500,
       crystals: 100
     },
-    upgrades: []
+    upgrades: [
+      {
+        label: 'Upgrade Science (lvl 1)',
+        code: 'lvl1',
+        isPurchased: false,
+        cost: {
+          gold: 2000,
+          iron: 50,
+          crystals: 50
+        }
+      },
+      {
+        label: 'Upgrade Science (lvl 2)',
+        code: 'lvl2',
+        isPurchased: false,
+        cost: {
+          gold: 4000,
+          iron: 100,
+          crystals: 75
+        }
+      },
+      {
+        label: 'Upgrade Science (lvl 3)',
+        code: 'lvl3',
+        isPurchased: false,
+        cost: {
+          gold: 15000,
+          iron: 500,
+          crystals: 200
+        }
+      },
+      {
+        label: 'Upgrade Science (lvl 4)',
+        code: 'lvl4',
+        isPurchased: false,
+        cost: {
+          gold: 30000,
+          iron: 750,
+          crystals: 500
+        }
+      },
+      {
+        label: 'Upgrade Science (lvl 5)',
+        code: 'lvl5',
+        isPurchased: false,
+        cost: {
+          gold: 75000,
+          iron: 1000,
+          crystals: 1000
+        }
+      }
+    ]
   },
 ];
 
